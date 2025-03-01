f6401248b9f333fe2ee9ac6c27d88efa933cf1e4
Alvarenga7 <kiteyyhastenreiter@gmail.com>
2025-02-28 22:46:55 -0300

    feat:Adicionado setup e componentes do shadcn

---
+++ src/app/products/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+const ProductsPage = () => {
+  return (
+    <div className="p-4">
+      <h1>Products Page</h1>
+      <Button>Alvarenga Mc Donalds</Button>
+      <Input placeholder="Bora começar" />
+    </div>
+  );
+};
+
+export default ProductsPage;
