1fe1cecf564e1417e156d80b81d98a60b0e4fc45
Alvarenga7 <kiteyyhastenreiter@gmail.com>
2025-02-28 22:57:42 -0300

    chore:adicionado plugin eslint e prettier do tailwind

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -3,7 +3,7 @@ import { Input } from "@/components/ui/input";
 
 const ProductsPage = () => {
   return (
-    <div className="p-4">
+    <div className="rounded-xl border-red-500 p-4">
       <h1>Products Page</h1>
       <Button>Alvarenga Mc Donalds</Button>
       <Input placeholder="Bora começar" />
